4b9fe68f5a9798f184180bae7d624696c2fc7917
Amalianrlt <amalianurlita97@gmail.com>
2021-02-07 23:02:11 +0700

    Animated container

---
+++ my_flutter/lib/11AnimatedContainer.dart
@@ -0,0 +1,36 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Random random = Random();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Animated Container'),),
+        body: Center(
+          child: GestureDetector(
+            onTap: (){
+              setState(() {});
+            },
+            child: AnimatedContainer(
+              color: Color.fromARGB(255, random.nextInt(256), random.nextInt(256), random.nextInt(256)),
+              duration: Duration(seconds: 1),
+              width: 50.0 + random.nextInt(101),
+              height: 50.0 + random.nextInt(101),),
+          )
+        ),
+      )
+    );
+  }
+}
+
