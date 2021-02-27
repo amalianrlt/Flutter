6b924cb9a68152c875317057d0aaa47e15e4dba0
Amalianrlt <amalianurlita97@gmail.com>
2021-02-27 23:34:44 +0700

    Learning stack and alignment

---
+++ my_flutter/lib/13StackAndAlignment.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(appBar: 
+      AppBar(
+        title: Text('Stack Align')),
+        body: Stack(
+          children: <Widget>[
+            Column(
+              children : <Widget>[
+                Flexible(
+                  flex: 1,
+                  child: Row( 
+                    children: <Widget> [
+                      Flexible(
+                        flex: 1,
+                        child: Container(
+                          color: Colors.teal,
+                      )),
+                      Flexible(
+                        flex: 1,
+                        child: Container(
+                          color: Colors.red,
+                      )),
+                    ],
+                  )
+                ),
+                Flexible(
+                  flex: 1,
+                  child: Row( 
+                    children: <Widget> [
+                      Flexible(
+                        flex: 1,
+                        child: Container(
+                          color: Colors.green,
+                      )),
+                      Flexible(
+                        flex: 1,
+                        child: Container(
+                          color: Colors.yellow  ,
+                      )),
+                    ],
+                  )
+                ),
+              ]
+            ),
+            ListView(
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Text(
+                      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Why do we use it? It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).', 
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    Text(
+                      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Why do we use it? It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for lorem ipsum will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).', 
+                      style: TextStyle(fontSize: 18)
+                    ),
+                  ]
+                )
+              ],
+            ),
+            Align(
+                alignment: Alignment(0, 0.9),
+                child: RaisedButton(
+                  child: Text('button'),
+                  color: Colors.purple,
+                  onPressed: (){}
+              ),
+            )
+          ]
+        ),
+        )
+    );
+  }
+}
