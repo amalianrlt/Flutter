617b746fba3ca11928f44472a364fea800559c62
Amalianrlt <amalianurlita97@gmail.com>
2021-02-28 18:53:32 +0700

    Playing with text field

--- my_flutter/lib/secondPage.dart
+++ my_flutter/lib/secondPage.dart
@@ -1,16 +1,34 @@
 import 'package:flutter/material.dart';
 
-class SecondPage extends StatelessWidget {
+class SecondPage extends StatefulWidget {
+  @override
+  _SecondPageState createState() => _SecondPageState();
+}
+
+class _SecondPageState extends State<SecondPage> {
+  TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Second Page')),
-      body: Center(
-        child: RaisedButton(
-          child: Text('back'),
-          onPressed: (){
-            Navigator.pop(context);
-          })
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+                hintText: 'Input your name',
+                prefixIcon: Icon(Icons.person)
+              ),
+              controller: controller,
+              onChanged: (value) {
+                setState(() {});
+              },
+            ),
+            Text(controller.text)
+          ],
+        ),
       )
     );
   }
