f333460d6990ddbee9f2c78b428cf47611104410
Amalianrlt <amalianurlita97@gmail.com>
2021-02-28 18:29:57 +0700

    Refactor card in mainpage

--- my_flutter/lib/mainPage.dart
+++ my_flutter/lib/mainPage.dart
@@ -5,28 +5,43 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.pink[100],
       appBar: AppBar(
         title: Text('Main Page'),
         leading: Icon(Icons.menu, color: Colors.white,),
         actions: <Widget>[
           IconButton(icon: Icon(Icons.photo_camera), color: Colors.white, onPressed: (){}),
-          IconButton(icon: Icon(Icons.exit_to_app), color: Colors.white, onPressed: (){}),
+          IconButton(icon: Icon(Icons.navigate_next), color: Colors.white, onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context){
+                return SecondPage();
+              }));
+            }),
         ],
         flexibleSpace: Container(
           decoration: BoxDecoration(
-            gradient : LinearGradient(colors: [Colors.deepPurple, Colors.yellow], begin: FractionalOffset.topLeft, end: FractionalOffset.bottomLeft)
+            gradient : LinearGradient(colors: [Colors.deepPurple, Colors.purple[100]], begin: FractionalOffset.topLeft, end: FractionalOffset.bottomLeft)
           ),
         ),
         ),
-      body: Center(
-        child: RaisedButton(
-          child: Text('go to second page'),
-          onPressed: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context){
-              return SecondPage();
-            }));
-          })
+      body: Container(
+        padding: EdgeInsets.all(5),
+        child: ListView(
+          children: <Widget>[
+            contactCard('Kenzo'),
+            contactCard('Quenzha'),
+          ]
+        ),
       )
     );
+  }
+
+  Card contactCard(String text) {
+    return Card(
+            child: Row(
+              children: <Widget>[
+                Container(margin: EdgeInsets.all(5), child: Icon(Icons.perm_contact_calendar),),
+                Text(text)
+              ],)
+          );
   }
 }
