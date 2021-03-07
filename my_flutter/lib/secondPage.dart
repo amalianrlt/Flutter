def7a67a7c4af6b88ccf1528e8a68a96bd2c84f3
Amalianrlt <amalianurlita97@gmail.com>
2021-03-07 19:45:22 +0700

    Add input text

--- my_flutter/lib/secondPage.dart
+++ my_flutter/lib/secondPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_flutter/thirdPage.dart';
 
 class SecondPage extends StatefulWidget {
   @override
@@ -10,26 +11,36 @@ class _SecondPageState extends State<SecondPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Second Page')),
-      body: Container(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-                hintText: 'Input your name',
-                prefixIcon: Icon(Icons.person)
+        appBar: AppBar(title: Text('Second Page')),
+        body: Container(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            children: <Widget>[
+              TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    hintText: 'Input your name',
+                    prefixIcon: Icon(Icons.person)),
+                controller: controller,
+                onChanged: (value) {
+                  setState(() {});
+                },
               ),
-              controller: controller,
-              onChanged: (value) {
-                setState(() {});
-              },
-            ),
-            Text(controller.text)
-          ],
-        ),
-      )
-    );
+              Text(controller.text),
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                child: RaisedButton(
+                    child: Text('Submit'),
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return ThirdPage();
+                      }));
+                    }),
+              )
+            ],
+          ),
+        ));
   }
 }
---
+++ my_flutter/lib/thirdPage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ThirdPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Media Query'),),
+      ),
+    );
+  }
+}
